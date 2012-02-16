dbf8c593d6202a3a2abc1716b5451171b02a8a18
Matt Atkins <matt@yoomee.com>
2012-02-16 10:51:49 +0000

    HTTP Digest Auth.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  http_basic_authenticate_with :name => "unseen", :password => "photo123"
+  before_filter :authenticate
+
+  AUTH_USERS = { "unseen" => "photo123" }
+
+  private
+  def authenticate
+    authenticate_or_request_with_http_digest do |username|
+      AUTH_USERS[username]
+    end
+  end
 end
