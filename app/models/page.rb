47dd30e0f0778b4dc9fb52045d7d0a432e2ad741
Ian Mooney <ian@yoomee.com>
2012-03-07 13:42:57 +0000

    Added gallery_fax and galler_facebook snippets

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,7 +7,7 @@ class Page < ActiveRecord::Base
   
   accepts_nested_attributes_for :slideshow, :reject_if => :all_blank
   
-  has_snippets :gallery_address, :gallery_phone, :gallery_email, :gallery_website, :gallery_hours
+  has_snippets :gallery_address, :gallery_phone, :gallery_fax, :gallery_email, :gallery_website, :gallery_facebook, :gallery_hours
   
   validates :gallery_email, :email => true, :allow_blank => true
   validates :gallery_website, :url => true, :allow_blank => true
