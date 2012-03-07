7f92637a7d9690e7f6b2450fd335ab50b584ad85
Ian Mooney <ian@yoomee.com>
2012-03-07 12:25:27 +0000

    List page children latest first on news page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,12 @@ class PagesController < ApplicationController
   
   include YmCms::PagesController
   
+  expose(:page_children) do
+    if page
+      page.slug=="news" ? page.children.order("created_at DESC") : page.children
+    else
+      Page.scoped
+    end
+  end
+  
 end
