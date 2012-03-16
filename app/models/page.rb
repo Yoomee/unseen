c23ec015bdcdfa6a3122335d5988069fbd433db6
Matt Atkins <matt@yoomee.com>
2012-03-16 16:03:26 +0000

    Deal with empty slideshows.

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,8 +4,8 @@ class Page < ActiveRecord::Base
   include YmCms::Page
   
   has_one :slideshow, :as => :attachable
-  
   accepts_nested_attributes_for :slideshow, :reject_if => :all_blank
+  before_save :delete_slideshow_if_no_slides
   
   has_snippets :gallery_address, :gallery_phone, :gallery_fax, :gallery_email, :gallery_website, :gallery_facebook, :gallery_hours
   
@@ -20,6 +20,11 @@ class Page < ActiveRecord::Base
   
   def has_gallery_contact_details?
     [gallery_address, gallery_phone, gallery_email, gallery_website].any?(&:present?)
+  end
+  
+  private
+  def delete_slideshow_if_no_slides
+    slideshow.mark_for_destruction if slideshow && slideshow.slides.size.zero?
   end
   
 end
