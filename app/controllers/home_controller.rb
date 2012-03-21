fd94844eaa6d01bae693e4fb0e249bf879c8a819
Rich Wells <rich@yoomee.com>
2012-03-21 15:38:41 +0000

    Added about page design, fixed font stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   expose(:news_page) { Page.find_by_slug("news") }
-  expose(:news_pages) { news_page ? news_page.children.order("created_at DESC").limit(4) : [] }
+  expose(:news_pages) { news_page ? news_page.children.order("created_at DESC").limit(3) : [] }
   
   def index
   end
--- app/models/page.rb
+++ app/models/page.rb
@@ -7,14 +7,14 @@ class Page < ActiveRecord::Base
   accepts_nested_attributes_for :slideshow, :reject_if => :all_blank
   before_save :delete_slideshow_if_no_slides
   
-  has_snippets :gallery_address, :gallery_phone, :gallery_fax, :gallery_email, :gallery_website, :gallery_facebook, :gallery_hours
+  has_snippets :gallery_address, :gallery_phone, :gallery_fax, :gallery_email, :gallery_website, :gallery_facebook, :gallery_hours, :text_second
   
   validates :gallery_email, :email => true, :allow_blank => true
   validates :gallery_website, :url => true, :allow_blank => true
   
   class << self
     def view_names
-      %w{basic tiled list gallery}
+      %w{basic tiled list gallery about}
     end
   end
   
