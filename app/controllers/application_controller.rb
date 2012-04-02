8cc2c9975eca1ceaa9cf453af07bc8df579e47b6
Matt Atkins <matt@yoomee.com>
2012-04-02 11:50:01 +0100

    HTTP Auth on staging.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   include YmUsers::ApplicationController
   protect_from_forgery
 
-  #before_filter :authenticate
+  before_filter :authenticate
 
   AUTH_USERS = { "unseen" => "photo123" }
 
   private
   def authenticate
-    return true unless Rails.env.production?
+    return true unless Rails.env.production? && request.host == "unseen.yoomee.com"
     authenticate_or_request_with_http_digest do |username|
       AUTH_USERS[username]
     end
