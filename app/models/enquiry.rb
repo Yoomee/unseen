cbcb59bedc3994e650845033416358d18f6cd601
Ian Mooney <ian@yoomee.com>
2012-05-30 11:09:10 +0100

    Added agree_to_terms to enquiry

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -4,8 +4,10 @@ class Enquiry < ActiveRecord::Base
   
   image_accessor :image
   file_accessor :legitimation
+  boolean_accessor :agree_to_terms
 
   validates :image, :legitimation, :length => {:maximum => 10.megabytes}, :allow_blank => true
   validates_property :format, :of => :image, :in => [:jpeg, :jpg, :png, :gif], :message => "must be an image"
+  validates :agree_to_terms, :presence => true
     
 end
