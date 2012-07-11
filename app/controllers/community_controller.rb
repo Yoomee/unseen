114cbf992d36168614f39f61b762d6d0fc49e158
Edward Andrews <edward@yoomee.com>
2012-07-11 14:37:18 +0100

    Community index with ajax more links to events, photos and activity

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -1,7 +1,20 @@
 class CommunityController < ApplicationController
-  
+
   def index
-    @activity_items = YmActivity::ActivityItem.includes(:resource).order("created_at DESC").paginate(:per_page => 6, :page => params[:page])
+    if request.xhr?
+      @items = case params[:item_type]
+      when "activity_item"
+        YmActivity::ActivityItem.includes(:user,:resource).paginate(:per_page => 6, :page => params[:page])
+      when "event"
+        Event.popular.paginate(:per_page => 6, :page => params[:page])
+      when "photo"
+        Photo.popular.paginate(:per_page => 6, :page => params[:page])
+      end
+    else
+      @activity_items = YmActivity::ActivityItem.includes(:user,:resource).paginate(:per_page => 6, :page => params[:page])
+      @photos = Photo.popular.paginate(:per_page => 6, :page => params[:page])
+      @events = Event.popular.paginate(:per_page => 6, :page => params[:page])
+    end
   end
-  
+
 end
