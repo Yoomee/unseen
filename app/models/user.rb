f5168377487b58f3ab76de6ad03e255a997dda96
Edward Andrews <edward@yoomee.com>
2012-07-11 12:29:33 +0100

    Add create user to activity stream

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,26 @@
 class User < ActiveRecord::Base
-  
+
   include YmUsers::User
-  
+
   has_many :photos, :foreign_key => "photographer_id"
   has_many :links, :as => :attachable, :class_name => "YmLinks::Link", :dependent => :destroy
   has_and_belongs_to_many :galleries, :class_name => "Page", :join_table => "galleries_photographers"
   has_and_belongs_to_many :events, :order => "starts_at DESC, date"
   has_and_belongs_to_many :favourite_photos, :class_name => "Photo", :join_table => "photos_users"
-  
+
+  after_create :record_activity  
+
   acts_as_taggable_on :user_tags
-  
+
   accepts_nested_attributes_for :photos, :links, :reject_if => :all_blank, :allow_destroy => true
-  
+
   def photographer?
     role_is?(:photographer)
   end
-  
+
+  private  
+  def record_activity  
+    record_activity!(self)  
+  end
+
 end
