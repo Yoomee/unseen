f52bb1bf4eef87065373d6ffc8912ac5c17c64b7
Edward Andrews <edward@yoomee.com>
2012-07-16 14:15:30 +0100

    It should be country not postcode!

--- db/migrate/20120716125757_add_location_info_to_user.rb
+++ db/migrate/20120716125757_add_location_info_to_user.rb
@@ -2,8 +2,8 @@ class AddLocationInfoToUser < ActiveRecord::Migration
   def change
     
     add_column :users, :city, :string
-    add_column :users, :postcode, :string
-    add_column :users, :lat, :string
-    add_column :users, :lng, :string
+    add_column :users, :country, :string
+    add_column :users, :lat, :float
+    add_column :users, :lng, :float
   end
 end
