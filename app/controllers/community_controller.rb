e11618089429ec0faa83fc9eb321a53af10fb7e5
Edward Andrews <edward@yoomee.com>
2012-07-16 11:25:58 +0100

    Update activity#image to look like new wireframe pic

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -14,7 +14,7 @@ class CommunityController < ApplicationController
       @activity_items = YmActivity::ActivityItem.includes(:user,:resource).paginate(:per_page => 6, :page => params[:page])
       @photos = Photo.popular.paginate(:per_page => 6, :page => params[:page])
       @events = Event.popular.paginate(:per_page => 6, :page => params[:page])
-      @tweets = view_context.latest_tweets('UnseenPhotoFair', :count => 30).first(4)
+      @tweets = view_context.latest_tweets('UnseenPhotoFair', :count => 30, :trim_user => false).first(4)
     end
   end
 
