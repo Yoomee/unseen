b5ddc47f934469e702d07f49bb8af866ae7f5701
Matt Atkins <matt@yoomee.com>
2012-07-26 09:24:18 +0100

    Mobile: Link to program

--- app/controllers/mobile/fair_controller.rb
+++ app/controllers/mobile/fair_controller.rb
@@ -3,12 +3,12 @@ class Mobile::FairController < MobileController
   def index
     @page_title = "Fair & Festival"
     @items = [
-      ["Full Programe", '#'],
-      ["Galleries", '#'],
-      ["Photographers", '#'],
-      ["Fair & Festival map", '#'],
-      ["Location", '#'],
-      ["Amsterdam", '#']
+      ["Full Programe", mobile_program_path],
+      ["Galleries", 'javascript:void(0);'],
+      ["Photographers", 'javascript:void(0);'],
+      ["Fair & Festival map", 'javascript:void(0);'],
+      ["Location", 'javascript:void(0);'],
+      ["Amsterdam", 'javascript:void(0);']
     ]
   end
   
