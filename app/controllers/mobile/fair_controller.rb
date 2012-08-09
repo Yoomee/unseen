c3468672fa3cb5ad95dfa6767533c6d6d6bbf3aa
Matt Atkins <matt@yoomee.com>
2012-08-09 10:18:17 +0100

    Mobile view nave bar tweak, no retina images, show fewer items in lists.

--- app/controllers/mobile/fair_controller.rb
+++ app/controllers/mobile/fair_controller.rb
@@ -3,13 +3,15 @@ class Mobile::FairController < MobileController
   def index
     @page_title = "Fair & Festival"
     @items = [
-      ["Full Programme", mobile_program_path],
-      ["Galleries", 'javascript:void(0);'],
-      ["Photographers", 'javascript:void(0);'],
-      ["Fair & Festival map", 'javascript:void(0);'],
-      ["Location", 'javascript:void(0);'],
-      ["Amsterdam", 'javascript:void(0);']
+      ["Full Programme", mobile_program_path]
     ]
+    # [
+    #   ["Galleries", 'javascript:void(0);'],
+    #   ["Photographers", 'javascript:void(0);'],
+    #   ["Fair & Festival map", 'javascript:void(0);'],
+    #   ["Location", 'javascript:void(0);'],
+    #   ["Amsterdam", 'javascript:void(0);']
+    # ]
   end
   
 end
