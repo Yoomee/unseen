edc2561c3b186e9533108d098477c7aa2d32920b
Matt Atkins <matt@yoomee.com>
2012-08-10 17:24:43 +0100

    Fix migration and event rightcol width

--- db/migrate/20120810152234_merge_duplicate_events.rb
+++ db/migrate/20120810152234_merge_duplicate_events.rb
@@ -1,6 +1,7 @@
 class MergeDuplicateEvents < ActiveRecord::Migration
   
   def self.up
+    Event.reset_column_information
     Event.order(:date).group(:title).each do |event|
       duplicate_events = Event.where(:title => event.title).without(event)
       if duplicate_events.present?
