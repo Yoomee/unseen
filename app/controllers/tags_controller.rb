2f583cdc392c443d5fff78db0e075f79086dd643
Edward Andrews <edward@yoomee.com>
2012-08-13 14:30:24 +0100

    On tag show, separate tagged users and other taggings

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,7 +4,11 @@ class TagsController < ApplicationController
   
   def show
     @taggings = current_tag.taggings.order("created_at DESC")
+    @user_taggings = @taggings.where(:taggable_type => 'User')
+    @other_taggings = @taggings.where('taggable_type != "User"')
     @taggables = @taggings.collect(&:taggable)
+    @user_taggables = @user_taggings.collect(&:taggable)
+    @other_taggables = @other_taggings.collect(&:taggable)
   end
   
 end
