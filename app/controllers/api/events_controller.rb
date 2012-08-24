7a934a4ad26f8d1478773d2a4914334311f88dc6
Ian Mooney <ian@yoomee.com>
2012-08-24 14:34:25 +0100

    Fix api_image_url and height for events

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,7 +1,7 @@
 class Api::EventsController < ApplicationController
   
   def index
-    render :json => Event.all.to_json(:methods => [:day1,:day2,:day3,:day4,:day5,:venue_name,:time_string,:image_url_for_api, :image_url_for_api])
+    render :json => Event.limit(3).to_json(:methods => [:day1,:day2,:day3,:day4,:day5,:venue_name,:time_string,:image_url_for_api, :image_height_for_api])
   end
   
   def show
