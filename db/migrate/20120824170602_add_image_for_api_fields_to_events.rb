f3258bbb33fb7fd260adcc111c3b47ca3bd5a150
Matt Atkins <matt@yoomee.com>
2012-08-29 17:42:20 +0100

    Fix migration

--- db/migrate/20120824170602_add_image_for_api_fields_to_events.rb
+++ db/migrate/20120824170602_add_image_for_api_fields_to_events.rb
@@ -2,11 +2,6 @@ class AddImageForApiFieldsToEvents < ActiveRecord::Migration
   def up
     add_column :events, :image_url_for_api, :string
     add_column :events, :image_height_for_api, :integer
-    Event.reset_column_information
-    Event.all.each do |e|
-      e.send(:set_api_image_fields)
-      e.save
-    end
   end
   
   def down
