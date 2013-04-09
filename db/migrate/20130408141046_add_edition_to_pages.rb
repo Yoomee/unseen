b89dd7e854722f3a324562dd2a9178368603bf03
Andy Mayer <andy@yoomee.com>
2013-04-09 02:56:39 +0100

    Set all current pages to 2012 edition

--- db/migrate/20130408141046_add_edition_to_pages.rb
+++ db/migrate/20130408141046_add_edition_to_pages.rb
@@ -3,6 +3,7 @@ class AddEditionToPages < ActiveRecord::Migration
     add_column :pages, :edition, :string
     add_column :pages, :gallery_parent_id, :integer
     Page.reset_column_information
+    Page.update_all(:edition => "2012")
     Page.find_by_slug(:galleries).update_attribute(:edition, '2012')
     Permalink.find_by_path(:galleries).update_attribute(:path, 'galleries-2012')
     Page.create(:title => 'Galleries 2013', :text => 'Galleries for 2013', :slug => 'galleries_2013', :view_name => :galleries, :edition => 2013, :user_id => 7, :permalink_path => 'galleries-2013')
