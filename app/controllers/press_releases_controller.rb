fab67f941dfb43d086bf7ab2ba10d8d9dcc57d56
Matt Atkins <matt@yoomee.com>
2013-04-11 11:37:57 +0100

    Add edit and destroy to press releases

--- app/controllers/press_releases_controller.rb
+++ app/controllers/press_releases_controller.rb
@@ -8,7 +8,20 @@ class PressReleasesController < ApplicationController
     end
   end
   
+  def destroy
+    @press_release.destroy
+    redirect_to press_releases_path
+  end
+  
   def show
     send_file @press_release.file.path, :filename => @press_release.file_name
+  end
+  
+  def update
+    if @press_release.update_attributes(params[:press_release])
+      redirect_to press_releases_path
+    else
+      render :action => 'edit'
+    end
   end
 end
