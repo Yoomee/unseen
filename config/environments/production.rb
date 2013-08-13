3923936bb6e1f54eb77c5b43810b6d09f9e38a1d
Matt Atkins <matt@yoomee.com>
2013-08-13 15:36:12 +0100

    Correct host!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Unseen::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'http://unseenamsterdam.com/' }
+  config.action_mailer.default_url_options = { :host => 'unseenamsterdam.com' }
   # TODO: uncomment this once DNS has changed
   # config.action_mailer.default_url_options = { :host => 'unseenamsterdam.com' }
 
