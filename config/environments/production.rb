72314a348b09e89b7533a731c908d6ea9e260307
Matt Atkins <matt@yoomee.com>
2013-08-13 15:20:27 +0100

    Correct host for links in emails (e.g password reminder)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Unseen::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'unseen.yoomee.com' }
+  config.action_mailer.default_url_options = { :host => 'http://unseenamsterdam.com/' }
   # TODO: uncomment this once DNS has changed
   # config.action_mailer.default_url_options = { :host => 'unseenamsterdam.com' }
 
