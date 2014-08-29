c1534c01f69e0b7c63dce28305cb30181062a6e7
Edward Andrews-Hodgson <edward@yoomee.com>
2014-08-29 16:39:54 +0100

    Set programme venue columns width automatically

--- app/assets/javascripts/programme.js
+++ app/assets/javascripts/programme.js
@@ -1,5 +1,23 @@
 var Programme = {
   init: function(){
+    $.each($('.venue_col'), function( index, value ) {
+      var venueCol = $(this);
+      var maxLeftPosition = 0;
+      $.each(venueCol.children('.session'), function(index, session) {
+        if ($(session).position().left > maxLeftPosition) {
+          maxLeftPosition = $(session).position().left;
+        }
+      });
+      venueCol.width(maxLeftPosition + 150);
+      venueCol.find('.col_title').width(maxLeftPosition + 150);
+    });
+    var width = 0;
+    $.each($('.venue_col'), function( index, value ) {
+      width += $(this).width();
+    });
+    width += 300; // A random number to make it wide enough!
+    $('#prog_wrapper .venue_cols').width(width);
+    Programme.minLeft = -width;
     $('.session').hover(
       function(){
         $('#prog_wrapper .session_hover').hide()
@@ -36,7 +54,7 @@ var Programme = {
         case Programme.minLeft:$('.prog_arrow_right').removeClass('active_arrow');$('.prog_arrow_left').addClass('active_arrow');break;
         default:$('.prog_arrow').addClass('active_arrow');break;
       }
-    }        
+    }
   },
   left: 0
 };
