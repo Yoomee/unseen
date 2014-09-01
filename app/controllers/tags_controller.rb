9a73820033c87a0ac67fc85fe29eebb8d90fc93d
Edward Andrews-Hodgson <edward@yoomee.com>
2014-09-01 09:26:08 +0100

    Only show photographers on premiere tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,6 +8,10 @@ class TagsController < ApplicationController
     @other_taggings = @taggings.where('taggable_type != "User"')
     @taggables = @taggings.collect(&:taggable)
     @user_taggables = @user_taggings.collect(&:taggable)
+    if current_tag == Tag.find_by_name('premiere')
+      user_ids = @user_taggables.collect(&:id)
+      @user_taggables = User.where('id IN (?) AND photographer_parent_id IS NOT NULL', user_ids)
+    end
     @other_taggables = @other_taggings.collect(&:taggable)
   end
   
